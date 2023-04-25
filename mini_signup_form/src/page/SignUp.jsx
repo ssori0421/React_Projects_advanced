377088c4ad17c842fdd3172a15612b156d7794c7
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-25 10:22:02 +0900

    email, password, passwordConform 값을 담을 state 생성 및 값 업데이트

--- mini_signup_form/src/page/SignUp.jsx
+++ mini_signup_form/src/page/SignUp.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+  const onEmailChange = (e) => {
+    setFormState({ ...formState, email: e.target.value });
+  };
+  const onPasswordChange = (e) => {
+    setFormState({ ...formState, password: e.target.value });
+  };
+  const onPasswordConfirmChange = (e) => {
+    setFormState({ ...formState, passwordConfirm: e.target.value });
+  };
+  console.log(formState);
+
   return (
     <div>
-      <input data-testid='email-input' />
-      <input data-testid='password-input' />
-      <input data-testid='password-confirm-input' />
+      <input data-testid='email-input' onChange={onEmailChange} />
+      <input data-testid='password-input' onChange={onPasswordChange} />
+      <input
+        data-testid='password-confirm-input'
+        onChange={onPasswordConfirmChange}
+      />
       <button data-testid='signup-button'>회원가입</button>
     </div>
   );
